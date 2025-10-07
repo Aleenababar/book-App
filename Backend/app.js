0e880d8f14cf33f320a25180cdd69b8ad99d5336
Aleenababar <Aleenababar471@gmai.com>
2025-10-07 19:44:56 +0800

    change done

--- Backend/app.js
+++ Backend/app.js
@@ -10,10 +10,7 @@ const path = require("path");
 // Serve frontend build (React)
 app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-// For SPA routing
-app.get("*", (req, res) => {
-  res.sendFile(path.join(__dirname, "../Frontend/build", "index.html"));
-});
+
 
 app.use (cors());
 app.use(express.json());
@@ -21,6 +18,10 @@ conn();
 app.use("/api/v1", UserAPI);
 // localhost:1000/api/v1/sign-in 
 app.use("/api/v2", TaskAPI);
+// For SPA routing
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../Frontend/build", "index.html"));
+});
 
 const PORT =1000;
 app.listen(PORT,()=>(console.log("server started")));
